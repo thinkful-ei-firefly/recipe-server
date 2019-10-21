af119ef72bb496a3a3ffaeb16df60b129eecdd6a
Sam Walker <jack.nasal@gmail.com>
2019-10-21 11:47:14 -0400

    updated config and example env to use database name

--- src/config.js
+++ src/config.js
@@ -1,6 +1,6 @@
 module.exports = {
   PORT: process.env.PORT || 8080,
   NODE_ENV: process.env.NODE_ENV || 'development',
-  DB_URL: process.env.DATABASE_URL || 'postresql://postgres@localhost/gameplay-valet',
+  DB_URL: process.env.DATABASE_URL || 'postresql://postgres@localhost/cooking',
   JWT_SECRET: process.env.JWT_SECRET || 'change-this-secret'
 }
