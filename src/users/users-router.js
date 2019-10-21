bcd6464f3302a3b17c8d960362c2a8bc46486b44
Sam Walker <jack.nasal@gmail.com>
2019-10-21 11:41:17 -0400

    added authentication

---
+++ src/users/users-router.js
@@ -0,0 +1,52 @@
+const express = require('express');
+const path = require('path');
+
+const UsersService = require('./users-service');
+const AuthService = require('../auth/auth-service')
+
+const usersRouter = express.Router();
+const jsonBodyParser = express.json();
+
+usersRouter
+  .post('/', jsonBodyParser, (req, res, next) => {
+    const { password, user_name } = req.body;
+    const requiredFields = ['user_name', 'password'];
+
+    for(const field of requiredFields) {
+      if (!req.body[field])
+        return res.status(400).json({ error: `Missing '${field}' in requrest` });
+    }
+
+    const passwordError = UsersService.checkPassword(password);
+    if(passwordError) {
+      return res.status(400).json({ error: passwordError });
+    }
+
+    UsersService.hasUserWithUserName(req.app.get('db'), user_name)
+      .then(userExists => {
+        if (userExists) {
+          return res.status(400).json({ error: 'That username is already taken' });
+        }
+        return UsersService.hashPassword(password)
+          .then(hashedPassword => {
+            const newUser = {
+              user_name,
+              password: hashedPassword,
+              date_created: 'now()'
+            }
+            return UsersService.insertUser(req.app.get('db'), newUser)
+              .then(user => {
+                const userInfo = UsersService.serializeUser(user)
+                const payload = { user_id: userInfo.id };
+                const subject = userInfo.user_name;
+                res
+                  .status(201)
+                  .location(path.posix.join(req.originalUrl, `/${user.id}`))
+                  .json({ authToken: AuthService.createJwt(subject, payload), id: userInfo.id})
+              })
+          })
+      })
+      .catch(next)
+  })
+
+  module.exports = usersRouter;
---
+++ src/users/users-service.js
@@ -0,0 +1,39 @@
+const xss = require('xss');
+const bcrypt = require('bcryptjs');
+
+const UsersService = {
+  checkPassword(password) {
+    if(password.startsWith(' ') || password.endsWith(' ')) {
+      return 'Password must not start or end with empty spaces'
+    }
+    if (password.length < 8 || password.length > 72) {
+      return 'Password must be between 8 and 72 characters'
+    }
+    return null
+  },
+  hasUserWithUserName(db, user_name) {
+    return db('users')
+      .where({ user_name })
+      .first()
+      .then(user => !!user)
+  },
+  insertUser(db, newUser) {
+    return db
+      .into('users')
+      .insert(newUser)
+      .returning('*')
+      .then(user => user[0])
+  },
+  serializeUser(user) {
+    return {
+      id: user.id,
+      user_name: xss(user.user_name),
+      date_created: new Date(user.date_created)
+    }
+  },
+  hashPassword(password) {
+    return bcrypt.hash(password, 10)
+  }
+}
+
+module.exports = UsersService;
