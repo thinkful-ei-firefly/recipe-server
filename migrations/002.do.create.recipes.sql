079141e943b7d16222921bf9a558b17a9292d653
Keith Prince <keithcprince@gmail.com>
2019-10-21 11:37:54 -0400

    first migrations

---
+++ migrations/002.do.create.recipes.sql
@@ -0,0 +1,22 @@
+CREATE TABLE recipes(
+  id SERIAL PRIMARY KEY,
+  owner INTEGER REFERENCES users(id),
+  name TEXT NOT NULL,
+  ingregients TEXT[],
+  instructions TEXT[],
+  date_added DATE DEFAULT GETDATE(),
+  time_to_make INTEGER
+);
+/*
+ingredients array setup: ["food item, amount", "food item, amount", etc]
+text array setup: [step 1, step 2, etc]
+*/
+
+CREATE TABLE ingredients(
+  id SERIAL PRIMARY KEY,
+  owner INTEGER REFERENCES users(id),
+  food TEXT[][]
+);
+/*
+food array setup: [food item, food item, etc] [date_added, amount]
+*/
---
+++ migrations/002.undo.create.recipes.sql
@@ -0,0 +1,3 @@
+DROP TABLE IF EXISTS recipes;
+
+DROP TABLE IF EXISTS ingredients;
